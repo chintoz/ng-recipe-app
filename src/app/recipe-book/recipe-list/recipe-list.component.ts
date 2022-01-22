d1ae4853f7048a6c84ee13ee98855958dc2fe2dc
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2022-01-22 01:07:03 +0100

    Changes to add Recipe Model and changes to start creating recipe list.

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('A Test Recipe', 'A Test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
